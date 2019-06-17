91d30f95d4d7f451b4dbbf65f1a8500ca1085baf
Adeel Ahmed Khan <adeelbarki@gmail.com>
2019-06-17 11:06:42 +0500

    Added status bar

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import MainNavigator from './components/Tabs'
+import MobileStatusBar from './components/Statusbar'
+import { purple } from './utils/colors'
 
 class App extends Component {
   render() {
     return (
       <View style={{ flex: 1 }}>
+        <MobileStatusBar backgroundColor={purple} barStyle='light-content' />
         <MainNavigator />
       </View>
     );
